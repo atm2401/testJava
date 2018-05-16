0e2fe91e4834a216ef3704870f2ad41c67933573
Tamsir Diop <tdiop@prestatech.biz>
2018-05-16 12:06:10 +0000

    first commit

---
+++ TestJava/src/sn/java/test/Afficher.java
@@ -0,0 +1,13 @@
+package sn.java.test;
+
+public class Afficher {
+
+	public static void afficher(){
+		System.out.println("Nom: DIOP Prénom: TAMSIR");
+	}
+	public static void main(String args[]){
+		
+		afficher();
+		
+	}
+}
